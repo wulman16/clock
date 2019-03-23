fef47711a38e1e2f1ad976645cdf79cd60c2550e
wulman16 <will.ulman@gmail.com>
2019-03-23 16:36:27 -0400

    create a cascade of clocks

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const first = document.createElement('h1')
-document.querySelector('body').append(first)
-
-function displayTime() {
-  first.textContent = new Date().toLocaleTimeString()
+for(let i = 0; i < 10; i++) {
+  const clock = document.createElement('h1')
+  clock.className = 'clock'
+  clock.id = i
+  clock.style.marginLeft = `${i*60}px`
+  document.querySelector('body').append(clock)
 }
 
-setInterval(displayTime, 1000)
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function displayTime(i, callback) {
+  setTimeout(function() {
+    const clock = document.getElementById(i)
+    clock.textContent = new Date().toLocaleTimeString()
+    if (i < 9) displayTime(++i, callback)
+    else callback(4)
+  }, 1000)
+}
+
+function updateTime(i) {
+  setTimeout(function() {
+    const clock = document.getElementById(i)
+    clock.textContent = new Date().toLocaleTimeString()
+    updateTime(getRandomInt(10))
+  }, 1000)
+}
+
+displayTime(0, updateTime)
